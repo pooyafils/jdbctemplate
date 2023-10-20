18116637b07bec6ff4607916c0b96c02b377f73f
pooyafils <pooyafils@gmail.com>
2023-10-20 15:25:17 +0330

    adding logging for application

--- src/main/java/com/example/jdbctemplate/contoller/TableInfoController.java
+++ src/main/java/com/example/jdbctemplate/contoller/TableInfoController.java
@@ -3,6 +3,8 @@ package com.example.jdbctemplate.contoller;
 import com.example.jdbctemplate.model.Employee;
 import com.example.jdbctemplate.repository.EmployeeRepository;
 import com.example.jdbctemplate.repository.TableInfoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +15,20 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class TableInfoController {
+    Logger logger =  LoggerFactory.getLogger(TableInfoController.class);
+
     @Autowired
     TableInfoRepository tableInfoRepository;
     @Autowired
     EmployeeRepository employeeRepository;
+    @GetMapping("/t")
+    public ResponseEntity createTable(){
+        logger.info("loggggggggggggggggggggggggginf");
+        return ResponseEntity.ok("table has been created");
+    }
     @GetMapping
     public ResponseEntity createTable(@RequestParam String tableName,@RequestParam String[] cloums){
-
+logger.info("loggggggggggggggggggggggggginf");
         tableInfoRepository.createTable(tableName,cloums);
         return ResponseEntity.ok("table has been created");
     }
